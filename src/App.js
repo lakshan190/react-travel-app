aa733124dec2036722a66bd08bb8117801aee41e
lakshan190 <lvratnaike90@gmail.com>
2023-06-27 16:59:59 +0530

    completed the stats component

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItems={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -129,10 +129,33 @@ function Item({ item, onDeleteItem, onToggleItems }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+
+  // you need to check the length and you cant check !items because the items state is an
+  // empty object at starting so empty objects are not falsy in js.
+
+  const numItems = items.length;
+  const numOfPacked = items.filter((item) => item.packed).length;
+  // item.packed === true
+  const percentage = Math.round((numOfPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+      {percentage === 100 ? (
+        <em>You got everything! Ready to go ✈️🌏</em>
+      ) : (
+        <em>
+          💼 You have {numItems} items on your list, and you already packed{" "}
+          {numOfPacked} ({percentage}%)
+        </em>
+      )}
+      {/* you can also use a string literal template but rendering a separate tag is better */}
     </footer>
   );
 }
